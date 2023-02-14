5bc59dc4bd06d78e8bc6e8329bc78dd1a42f42f5
vishalRamlogan <59810959+vishalramlogan@users.noreply.github.com>
2023-02-13 23:53:39 -0400

    Add files via upload

---
+++ users.service.ts
@@ -0,0 +1,141 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from './user.model';
+import { Router } from '@angular/router';
+import  axios  from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+  errorMessage = new EventEmitter<string>();
+  MessageSQ = new EventEmitter<string>();  
+  Response = new EventEmitter<[]>();  
+
+  private users: User[] = [];
+  private usersAdd = new Subject<User[]>();
+
+  private token: string;  
+
+  constructor(private http: HttpClient,private router: Router) { 
+    this.token = localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+  }
+
+  addUserdb(username: string, password: string, passwordConfirmation: string, securityQuestion: string, securityQuestionAnswer: string){
+    const user: User = {username: username, password: password, passwordConfirmation: passwordConfirmation,
+       securityQuestion: securityQuestion, securityQuestionAnswer: securityQuestionAnswer};
+
+       this.http.post<{message: string,token: string}>("http://localhost:3000/api/signup", user).subscribe(
+         (response) => {
+           this.users.push(user);
+           this.usersAdd.next([...this.users]);
+           localStorage.setItem('token', response.token);
+           localStorage.setItem('theUser', username);
+           this.router.navigate(['', username]);
+         },
+         error => {
+           if (error.status === 409) {
+             this.errorMessage.emit(error.error.message);
+           }
+         } 
+       );
+  }
+
+  logindb(username: string, password: string,){
+    const passon = {username: username, password: password};
+
+    this.http.post<{token: string}>("http://localhost:3000/api/login", passon).subscribe(
+         (response) => {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('theUser', username);
+          if (username == 'Administrator'){
+            this.router.navigate(['admin']);
+          }else{
+            this.router.navigate(['usersinfo', username]);
+          }
+         },
+         error => {
+           if (error.status === 401) {
+             this.errorMessage.emit(error.error.message);
+           }
+         }
+       );
+  }
+ 
+  passwordResetdb(username: string, password: string, passwordConfirmation: string, securityQuestionAnswer: string){
+    const passon = {username: username, password: password, passwordConfirmation: passwordConfirmation,
+       securityQuestionAnswer: securityQuestionAnswer};
+
+       this.http.put<{token: string}>("http://localhost:3000/api/users", passon).subscribe(
+         (response) => {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('theUser', username);
+          this.router.navigate(['', username]);
+         },
+         error => {
+           if (error.status === 401) {
+             this.errorMessage.emit(error.error.message);
+           }
+         }
+       );
+  }
+
+  passwordResetdbSQ(username: string){
+       this.http.get<{messageSQD: string}>("http://localhost:3000/api/users?username="+username)
+       .subscribe(messageData => {
+        this.MessageSQ.emit(messageData.messageSQD);
+      });
+  }
+
+  getUserInfo(){
+    const username = localStorage.getItem('theUser');
+    const headers = {'Authorization': `Bearer ${this.token}`};
+    this.http.get<{mappings: []}>(("http://localhost:3000/api/usersinfo?username="+username),{ headers })
+    .subscribe(messageData => {
+      this.Response.emit(messageData.mappings);
+    }); 
+}
+
+getUserInfoAdmin(username: string){
+  const headers = {'Authorization': `Bearer ${this.token}`};
+  this.http.get<{mappings: []}>(("http://localhost:3000/api/usersinfo?username="+username),{ headers })
+  .subscribe(messageData => {
+    this.Response.emit(messageData.mappings);
+  }); 
+}
+
+getListUsers(){
+  const username = localStorage.getItem('theUser');
+  const headers = {'Authorization': `Bearer ${this.token}`};
+  console.log("http://localhost:3000/api/"+username)
+  this.http.get<{username: []}>(("http://localhost:3000/api/users/"+username),{ headers })
+  .subscribe(messageData => {
+    this.Response.emit(messageData.username);
+  }); 
+}
+
+deletedb(username: string){
+  const headers = {'Authorization': `Bearer ${this.token}`};
+  const options = {
+    body: { username: username }
+  };
+  this.http.delete<{}>("http://localhost:3000/api/users/Administrator", { headers, ...options }).subscribe(
+    () => {
+      location.reload()
+    },
+    (error) => {
+      if (error.status === 401) {
+        alert(error.error.message);
+      }
+    }
+  );
+}
+
+  getUsersUpdateListner(){
+    return this.usersAdd.asObservable();
+  }
+
+}
